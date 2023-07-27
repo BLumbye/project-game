0bd0c3ef8301ca899fc9071cdcf1b1fd3532413e
Benjamin Lumbye <benjamin@lumbye.net>
2023-07-27 10:35:56 +0200

    Fixed no negative numbers bug

--- src/utils/validation/numbers.ts
+++ src/utils/validation/numbers.ts
@@ -3,42 +3,42 @@
  */
 export const isNumber = () => (element: string) => {
   return !isNaN(Number(element)) || element === '-' || element === '.' || element === '-.';
-}
+};
 
 /**
  * A predicate that sees if an element is a whole number. To be used together with array functions, such as `count` or `filter` or validation.
  */
 export const isWholeNumber = () => (element: string) => {
-  return !isNaN(Number(element)) && !element.includes('.');
-}
+  return (!isNaN(Number(element)) && !element.includes('.')) || element === '-';
+};
 
 /**
  * A predicate that sees if an element is positive. To be used together with array functions, such as `count` or `filter` or validation.
  */
 export const isPositive = () => (element: number) => {
   return (Number(element) || 0) >= 0;
-}
+};
 
 /**
  * A predicate that sees if an element is negative. To be used together with array functions, such as `count` or `filter` or validation.
  */
 export const isNegative = () => (element: number) => {
   return (Number(element) || 0) <= 0;
-}
+};
 
 /**
  * A predicate that sees if an element is strictly positive. To be used together with array functions, such as `count` or `filter` or validation.
  */
 export const isStrictlyPositive = () => (element: number) => {
   return (Number(element) || 0) > 0;
-}
+};
 
 /**
  * A predicate that sees if an element is strictly negative. To be used together with array functions, such as `count` or `filter` or validation.
  */
 export const isStrictlyNegative = () => (element: number) => {
   return (Number(element) || 0) < 0;
-}
+};
 
 /**
  * A predicate that sees if a number is less than another number. To be used together with array functions, such as `count` or `filter` or validation.
@@ -46,7 +46,7 @@ export const isStrictlyNegative = () => (element: number) => {
  */
 export const isLessThan = (value: number) => (element: number) => {
   return (Number(element) || 0) < value;
-}
+};
 
 /**
  * A predicate that sees if a number is less than or equal to another number. To be used together with array functions, such as `count` or `filter` or validation.
@@ -54,7 +54,7 @@ export const isLessThan = (value: number) => (element: number) => {
  */
 export const isLessThanOrEqualTo = (value: number) => (element: number) => {
   return (Number(element) || 0) <= value;
-}
+};
 
 /**
  * A predicate that sees if a number is greater than another number. To be used together with array functions, such as `count` or `filter` or validation.
@@ -62,7 +62,7 @@ export const isLessThanOrEqualTo = (value: number) => (element: number) => {
  */
 export const isGreaterThan = (value: number) => (element: number) => {
   return (Number(element) || 0) > value;
-}
+};
 
 /**
  * A predicate that sees if a number is greater than or equal to another number. To be used together with array functions, such as `count` or `filter` or validation.
@@ -70,7 +70,7 @@ export const isGreaterThan = (value: number) => (element: number) => {
  */
 export const isGreaterThanOrEqualTo = (value: number) => (element: number) => {
   return (Number(element) || 0) >= value;
-}
+};
 
 /**
  * A predicate that sees if the number is between two other numbers (both inclusive). To be used together with array functions, such as `count` or `filter` or validation.
@@ -79,7 +79,7 @@ export const isGreaterThanOrEqualTo = (value: number) => (element: number) => {
  */
 export const isBetween = (min: number, max: number) => (element: number) => {
   return (Number(element) || 0) >= min && (Number(element) || 0) <= max;
-}
+};
 
 /**
  * A predicate that sees if the number is between two other numbers (both exclusive). To be used together with array functions, such as `count` or `filter` or validation.
@@ -88,7 +88,7 @@ export const isBetween = (min: number, max: number) => (element: number) => {
  */
 export const isBetweenExclusive = (min: number, max: number) => (element: number) => {
   return (Number(element) || 0) > min && (Number(element) || 0) < max;
-}
+};
 
 /**
  * A predicate that sees if the number is between two other numbers (minimum inclusive, maximum exclusive). To be used together with array functions, such as `count` or `filter` or validation.
@@ -97,7 +97,7 @@ export const isBetweenExclusive = (min: number, max: number) => (element: number
  */
 export const isBetweenInclusiveExclusive = (min: number, max: number) => (element: number) => {
   return (Number(element) || 0) >= min && (Number(element) || 0) < max;
-}
+};
 
 /**
  * A predicate that sees if the number is between two other numbers (minimum exclusive, maximum inclusive). To be used together with array functions, such as `count` or `filter` or validation.
@@ -106,4 +106,4 @@ export const isBetweenInclusiveExclusive = (min: number, max: number) => (elemen
  */
 export const isBetweenExclusiveInclusive = (min: number, max: number) => (element: number) => {
   return (Number(element) || 0) > min && (Number(element) || 0) <= max;
-}
+};
